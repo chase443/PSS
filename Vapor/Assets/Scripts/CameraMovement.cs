387eead8b6d2ee54cabea4ee4920d887a6e8416c
Chase Davis <chase.davis@Chases-MacBook-Pro.local>
2015-11-29 15:22:57 -0500

    Initial commit

---
+++ Vapor/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public GameObject player;
+	private Vector2 velocity;
+
+	public float smoothTimeY;
+	public float smoothTimeX;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void FixedUpdate(){
+		float posX = Mathf.SmoothDamp (transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+		float posY = Mathf.SmoothDamp (transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+
+		transform.position = new Vector3 (posX,posY, transform.position.z);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (player.);
+	}
+}
---
+++ Vapor/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 3.0f;
+	public float gravity = -5f;
+	public float jumpSpeed = 10f;
+	private bool _isGrounded = true;
+
+	private float _vertSpeed;
+	public float terminalVelocity = -10f;
+
+	private CharacterController _charController;
+
+	// Use this for initialization
+	void Start () {
+		_charController = GetComponent<CharacterController> ();
+		_vertSpeed = gravity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float deltaX = Input.GetAxis ("Horizontal") * speed;
+		Vector3 movement = new Vector3 (deltaX, 0, 0);
+		movement = Vector3.ClampMagnitude (movement, speed);
+
+		if (_charController.isGrounded) {
+			if (Input.GetButtonDown ("Jump")) {
+				_vertSpeed = jumpSpeed;
+				_isGrounded = false;
+			} else {
+				_vertSpeed = -1.5f;
+			}
+		} else {
+			_vertSpeed += gravity * 5 * Time.deltaTime;
+			if(_vertSpeed < terminalVelocity){
+				_vertSpeed = terminalVelocity;
+			}		
+		}
+
+
+		movement.y = _vertSpeed;
+
+		movement *= Time.deltaTime;
+		movement = transform.TransformDirection (movement);
+		_charController.Move (movement);
+
+	}
+}
